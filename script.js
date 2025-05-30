0d093a79f2d36560e137097f9eaf933d3cf640b4
Renugadevi <renugaseenivasan135@gmail.com>
2025-05-30 12:44:22 +0530

    thirteen commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Mobile Navbar Toggle
+const toggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
